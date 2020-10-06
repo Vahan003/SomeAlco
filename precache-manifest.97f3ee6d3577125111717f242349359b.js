4b814359f95d6dd94dc479e0bdc95247de3f6bc2
Your Name <you@example.com>
2020-10-06 21:40:07 +0400

    Updates

---
+++ precache-manifest.97f3ee6d3577125111717f242349359b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7ae0135beee6735258767377d55b8eb",
+    "url": "/SomeAlco/index.html"
+  },
+  {
+    "revision": "5f27ee5aa745fb87ecbc",
+    "url": "/SomeAlco/static/css/main.4b048947.chunk.css"
+  },
+  {
+    "revision": "db4b4d00c90e440b75e7",
+    "url": "/SomeAlco/static/js/2.485ceec3.chunk.js"
+  },
+  {
+    "revision": "4cd4bb82c3efd9af5440b12fb6d92f3f",
+    "url": "/SomeAlco/static/js/2.485ceec3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f27ee5aa745fb87ecbc",
+    "url": "/SomeAlco/static/js/main.8037ab67.chunk.js"
+  },
+  {
+    "revision": "96b317e19212a3c775f2",
+    "url": "/SomeAlco/static/js/runtime-main.0cd3acfa.js"
+  },
+  {
+    "revision": "6678478931546f5d6c92e7b9f6f076cc",
+    "url": "/SomeAlco/static/media/loading.66784789.gif"
+  },
+  {
+    "revision": "045f21ed6d1626604a8822a57b700b41",
+    "url": "/SomeAlco/static/media/seedling.045f21ed.svg"
+  },
+  {
+    "revision": "29def3e5dd923346cc9efa53b9bc0b65",
+    "url": "/SomeAlco/static/media/user-regular.29def3e5.svg"
+  },
+  {
+    "revision": "495847b8234458c75fddf7f08f742845",
+    "url": "/SomeAlco/static/media/wine-bottle-solid.495847b8.svg"
+  }
+]);
